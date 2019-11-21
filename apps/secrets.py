b7928bf485b122b67705fe875c5ae4554faf7bc9
Gianpiero <gpammendola@yahoo.it>
2019-11-21 15:36:48 +0100

    Add files via upload

---
+++ apps/secrets.py
@@ -0,0 +1,9 @@
+secret_dict = {
+    "secret_work": "Siena, Italy",
+    "secret_home": "Siena, Italy",
+    "secret_device_user_one": "device_tracker.me",
+    "secret_device_user_two": "device_tracker.notme",
+    "secret_name_user_one": "me",
+    "secret_name_user_two": "notme",
+    "secret_ariela_mqtt": "homeassistant/tts/android_oneplus_5t_tts/tts"
+}
