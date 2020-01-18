cdcf984a392553f5259eebce41e6867c4c558d02
Gianpiero <gpammendola@yahoo.it>
2020-01-18 08:14:48 +0100

    Update phone_manager.py

--- apps/notifier/phone_manager.py
+++ apps/notifier/phone_manager.py
@@ -10,14 +10,19 @@ __NOTIFY__ = "notify/"
 
 class Phone_Manager(hass.Hass):
     def initialize(self):
-        self.no_op = 1
+        self.dict_lingua = {
+        "it": "it-IT-Standard-A",
+        "en": "en-GB-Standard-A",
+        "fr": "fr-RF-Standard-A",
+        "de": "de-DE-Standard-A",
+        "nl": "nl-NL-Standard-A"
+        }
 
     def send_voice_call(self, data, phone_name: str, sip_server_name: str):
         message = data["message_tts"].replace("\n","").replace("   ","").replace("  "," ").replace("_"," ")
         message_tts = message.replace(" ","%20")
         called_number= data["called_number"]
-        self.log("[MESSAGGIO]: {}".format(message))
-        self.log("[CALLED NUMBER]: {}".format(called_number))
+        lang = self.dict_lingua.get(data["language"])
         if called_number != "":
             if phone_name.find("voip_call") != -1:
                 called_number = ("sip:{}@{}".format(called_number, sip_server_name))
@@ -26,5 +31,5 @@ class Phone_Manager(hass.Hass):
                         input = {"call_sip_uri":called_number,"message_tts":message}
                         )
             else:
-                url_tts = ("http://api.callmebot.com/start.php?user={}&text={}&lang=it-IT-Standard-A".format(called_number, message_tts))
+                url_tts = ("http://api.callmebot.com/start.php?user={}&text={}&lang={}".format(called_number, message_tts,lang))
                 self.call_service("shell_command/telegram_call", url = url_tts)
