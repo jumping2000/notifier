8140f00087a2c3e7fb06439f030fa720864202fb
Gianpiero <gpammendola@yahoo.it>
2020-01-24 20:44:03 +0100

    Add files via upload

--- apps/notifier/alexa_manager.py
+++ apps/notifier/alexa_manager.py
@@ -6,26 +6,30 @@ import sys
 from queue import Queue
 from threading import Thread
 import re
-
 """
-Class Alexa Manager handles sending text to speech messages to Alexa media players
-Following features are implemented:
-- Speak text to choosen media_player
-- Full queue support to manage async multiple TTS commands
-- Full wait to tts to finish to be able to supply a callback method
+    Class Alexa Manager handles sending text to speech messages to Alexa media players
+    Following features are implemented:
+    - Speak text to choosen media_player
+    - Full queue support to manage async multiple TTS commands
+    - Full wait to tts to finish to be able to supply a callback method
 """
 
 __NOTIFY__ = "notify/"
+SUB_STRING = {"/h": " all'ora","$": "dollaro ", "€": "euro ","°C": " gradi","%": " per cento"}
+SUB_ALEXA = [("[\s_]+", " "),("[?!;:,]+", ", "),("\.+", ". "),("[\n\*]", "")]
 
 class Alexa_Manager(hass.Hass):
 
     def initialize(self) -> None:
+        # self.depends_on_module(globals)
         self.wait_time = globals.get_arg(self.args, "wait_time")
         self.queue = Queue(maxsize=0)
         self._when_tts_done_callback_queue = Queue()
         t = Thread(target=self.worker)
         t.daemon = True
         t.start()
+        self.version = self.get_ad_version()
+        self.log(f"Versione AppDaemon: {self.version}")
 
     def speak(self, data, alexa_notifier: str):
         """ SPEAK THE PROVIDED TEXT THROUGH THE MEDIA PLAYER """
@@ -34,7 +38,7 @@ class Alexa_Manager(hass.Hass):
         if self.queue.qsize() == 0:
             self.volume_get(data["media_player_alexa"],default_restore_volume)
         message = data["message_tts"]
-
+        
         """ Queues the message to be handled async, use when_tts_done_do method to supply callback when tts is done """
         self.queue.put({"title": data["title"], "text": message, "volume": data["volume"], "alexa_player": data["media_player_alexa"], 
                         "alexa_type": data["alexa_type"], "wait_time": wait_time, "alexa_method": data["alexa_method"], "alexa_notifier": alexa_notifier})
@@ -44,22 +48,22 @@ class Alexa_Manager(hass.Hass):
         restore_volume = volume
         if 'group' in media_player:
             list_player = self.get_state(media_player, attribute="entity_id")
-            # self.log("MEDIA PLAYER GROUP= {}".format(list_player))
+            # self.log(f"MEDIA PLAYER GROUP= {list_player}")
         else:
             list_player = self.converti(media_player)
-            # self.log("MEDIA PLAYER SINGLE: {}".format(list_player))
+            # self.log(f"MEDIA PLAYER SINGLE: {list_player}")
         for i in list_player:
-            self.dict_volumes[i] = self.get_state(entity_id = i, attribute="volume_level") or restore_volume
-        # self.log("GET VOLUMES: {}".format(self.dict_volumes))
+            self.dict_volumes[i] = self.get_state(i, attribute="volume_level", default=restore_volume) if self.version >= '4.0.0' else restore_volume
+        # self.log(f"GET VOLUMES: {self.dict_volumes}")
         return self.dict_volumes
 
     def volume_set(self, media_player, volume: float):
         if 'group' in media_player:
             list_player = self.get_state(media_player, attribute="entity_id")
-            # self.log("SET GRUPPO MEDIA_PLAYER/VOLUME: {} / {}".format(list_player,volume))
+            # self.log(f"SET GRUPPO MEDIA_PLAYER/VOLUME: {list_player} / {volume}")
             self.call_service("media_player/volume_set", entity_id = list_player, volume_level = volume)
         else:
-            # self.log("SET MEDIA_PLAYER/VOLUME: {} / {}".format(media_player,volume))
+            # self.log(f"SET MEDIA_PLAYER/VOLUME: {media_player} / {volume}")
             self.call_service("media_player/volume_set", entity_id = media_player, volume_level = volume)
 
     def replace(self, string, substitutions):
@@ -98,34 +102,31 @@ class Alexa_Manager(hass.Hass):
                         alexa_player = "group.hub_media_player_alexa"
 
                     """ REPLACE """
-                    string = data["text"]
-                    # Test def 
-                    substitutions = {"/h": " all'ora","$": "dollaro ", "€": "euro ","°C": " gradi","%": " per cento"}
-                    output = self.replace(string, substitutions)
-                    # Test for
-                    replacements = [("[\s_]+", " "),("[?!;:]+\s", ", "),("\.+\s", ". "),("[\n\*]", "")]
-                    for old, new in replacements:
-                        output = re.sub(old, new, output)
-                    # self.log("PRIMA: {}".format(string))
-                    # self.log("RISULTATO: {}".format(output))
+                    message_clean = self.replace(data["text"], SUB_STRING)
+                    for old, new in SUB_ALEXA:
+                        message_clean = re.sub(old, new, message_clean)
+                    # self.log(f"PRIMA: {data['text']}")
+                    # self.log(f"RISULTATO: {message_clean}")
 
                     """ SPEECH TIME CALCULATOR """
-                    period = output.count(', ') + output.count('. ') ##+ output.count(' - ')
-                    words = len(output.split())
-                    chars = output.count('')
+                    period = message_clean.count(', ') + message_clean.count('. ') ##+ message_clean.count(' - ')
+                    words = len(message_clean.split())
+                    chars = message_clean.count('')
                     
                     """ ESTIMATED TIME """
                     if (chars/words) > 7 and chars > 90:
                         data["wait_time"] += 7
-                        # self.log("ADD EXTRA TIME: {}".format(data["wait_time"]), level="WARNING")
+                        # self.log(f"ADD EXTRA TIME: {data["wait_time"]}"), level="WARNING")
                     # duration = ((chars * 0.00133) * 60) + data["wait_time"] #+ (period/2)
-                    # duration1 = (len(output.split()) / 2) + data["wait_time"]
-                    # duration2 = ((len(output.split()) / 130) * 60) + data["wait_time"]
+                    # duration1 = (len(message_clean.split()) / 2) + data["wait_time"]
+                    # duration2 = ((len(message_clean.split()) / 130) * 60) + data["wait_time"]
                     duration3 = ((words * 0.007) * 60) + data["wait_time"] #+ (period*0.2)
-                    # self.log("\n| DURATION     | PERIODO = {} | PAROLE = {} | CHARS = {} \n| (Char*0.00133) = {} \n| (OLD) = {} \n| (char/130) = {} \n| (Parole*0.007) = {}".format(period,words,chars,round(duration,2),round(duration1,2),round(duration2,2),round(duration3,2)))
+                    # self.log(f"\n| DURATION     | PERIODO = {period} | PAROLE = {words} | CHARS = {chars} \n| \
+                    #     (Char*0.00133) = {round(duration,2)} \n| (OLD) = {round(duration1,2)} \n| \
+                    #     (char/130) = {round(duration2,2)} \n| (Parole*0.007) = {round(duration3,2)}")
 
                     """ SPEAK """
-                    self.call_service(__NOTIFY__ + data["alexa_notifier"], data = alexa_data, target = alexa_player, message = output)
+                    self.call_service(__NOTIFY__ + data["alexa_notifier"], data = alexa_data, target = alexa_player, message = message_clean)
                     self.volume_set(alexa_player, data["volume"])
 
                     """ SLEEP AND WAIT FOR THE TTS TO FINISH """
@@ -142,7 +143,7 @@ class Alexa_Manager(hass.Hass):
                 if self.dict_volumes:
                     for i,j in self.dict_volumes.items():
                         self.call_service("media_player/volume_set", entity_id = i, volume_level = j)
-                        self.log("VOLUME RIPROGRAMMATO: {} - {}".format(j,i))
+                        self.log(f"VOLUME RIPROGRAMMATO: {j} - {i}")
                         # Force Set state
                         self.set_state(i, state = "", attributes = {"volume_level": j})
                 # It is empty, make callbacks
