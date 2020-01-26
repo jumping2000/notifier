71e98f516d51cf005e47329c8e880a0821d71bc4
Gianpiero <gpammendola@yahoo.it>
2020-01-26 19:27:15 +0100

    Add files via upload

--- apps/notifier/notification_manager.py
+++ apps/notifier/notification_manager.py
@@ -1,4 +1,4 @@
-import appdaemon.plugins.hass.hassapi as hass
+import hassapi as hass
 import datetime
 import globals
 
@@ -6,22 +6,23 @@ import globals
 Class Notification_Manager handles sending text to notfyng service
 """
 __NOTIFY__ = "notify/"
+SUB_NOTIFICHE = [("[\s]+"," ")]
 
 class Notification_Manager(hass.Hass):
 
     def initialize(self):
         self.text_last_message = globals.get_arg(self.args, "text_last_message")
 
-    def send_notify(self, data, notify_name: str, assistant_name: str):
+    def send_notify(self, data, notify_name: str, notify_alexa: str, assistant_name: str):
         timestamp = datetime.datetime.now().strftime("%H:%M:%S")
         title = data["title"]
-        message = data["message"].replace("\n","").replace("   ","").replace("  "," ").replace("_"," ")
+        message = globals.replace_regular(data["message"], SUB_NOTIFICHE).replace("_","\_")
         url = data["url"]
         _file = data["file"]
         caption = data["caption"]
         link = data["link"]
-        #self.log("[MESSAGGIO]: {}".format(message))
-        #self.log("[Notifier]: {}".format(notify_name))
+        #self.log("[MESSAGGIO]: {}".format(message), ascii_encode = False)
+        #self.log("[Notifier]: {}".format(notify_name), ascii_encode = False)
         if (data["notify"] != ""):
             notify_name = data["notify"]
         ### SAVE IN INPUT_TEXT.LAST_MESSAGE
@@ -50,11 +51,13 @@ class Notification_Manager(hass.Hass):
                             {"file": _file,
                             "caption": caption}
                         }
-        if url !="" or _file !="":
+        if url != "" or _file != "":
             self.call_service(__NOTIFY__ + notify_name,
                             message = "",
                             data = extra_data)
-        elif (url =="" and _file =="") and (notify_name.find("alexa") != -1 or data["alexa_push"] =="1"):
+        elif (url == "" and _file == "") and (notify_name.find("alexa") != -1 or data["alexa_push"] == "1"):
+            notify_name = notify_alexa
+            #self.log("[Notifier]: {}".format(notify_name), ascii_encode = False)
             self.call_service(__NOTIFY__ + notify_name, 
                             data = {"type": "push"}, 
                             target = data["media_player_alexa"], 
@@ -72,7 +75,8 @@ class Notification_Manager(hass.Hass):
         except:
             per_not_info = "null"
             #self.log(sys.exc_ingo())
-        message = data["message"].replace("\n","").replace("   ","").replace("  "," ").replace("_"," ")
+        #message = data["message"].replace("\n","").replace("   ","").replace("  "," ").replace("_"," ")
+        message = globals.replace_regular(data["message"], SUB_NOTIFICHE)
         message = ("{} - {}".format(timestamp, message))
         if per_not_info == "notifying":
             message = self.get_state(persistent_notification_info, attribute="message") + "\n" + message
