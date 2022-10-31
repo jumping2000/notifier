018d6102c5d243faa899004f5722421456f1c1c5
Gianpiero <gpammendola@yahoo.it>
2022-10-31 11:19:37 +0100

    dev 31/10

--- apps/notifier/helpermodule.py
+++ apps/notifier/helpermodule.py
@@ -49,12 +49,12 @@ def remove_key(d, key)->dict:
     del r[key]
     return r
 
-# """check if is an array """
-def check_array(target):
+# """check if is an array and returns a list is Input is a text"""
+def return_array(target)->list:
     if isinstance(target, list):
         return target
     else:
-        return list(target.replace(" ","").split(","))
+        return list(target.split(","))
         
 def replace_regular(text: str, substitutions: list)->str:
     for old,new in substitutions:
