20d05427bdaf0ed9610854ffc85af0ff0200d5f1
caiosweet <24454580+caiosweet@users.noreply.github.com>
2022-11-17 18:21:32 +0100

    Fixed language in phone manager app

--- apps/notifier/phone_manager.py
+++ apps/notifier/phone_manager.py
@@ -15,6 +15,7 @@ SUB_TTS = [("[\*\-\[\]_\(\)\{\~\|\}\s]+", " ")]
 class Phone_Manager(hass.Hass):
     def initialize(self):
         self.tts_language = self.args.get("tts_language")
+        self.log(f"lang: {self.tts_language}")
         self.dict_lingua = {
             "it-IT": "it-IT-Standard-A",
             "en-GB": "en-GB-Standard-A",
@@ -28,7 +29,7 @@ class Phone_Manager(hass.Hass):
         message = h.replace_regular(data["message"], SUB_TTS)
         message_tts = message.replace(" ", "%20")
         called_number = data["called_number"]
-        lang = self.dict_lingua.get(self.tts_language)
+        lang = self.dict_lingua.get(self.get_state(self.tts_language))
         phone_name = phone_name.lower().replace(" ", "_")
         if phone_name.find("voip_call") != -1:
             if called_number != "":
@@ -44,3 +45,4 @@ class Phone_Manager(hass.Hass):
                     called_number, message_tts, lang
                 )
                 self.call_service("shell_command/telegram_call", url=url_tts)
+                self.log(f"telegtam call: {url_tts}")
